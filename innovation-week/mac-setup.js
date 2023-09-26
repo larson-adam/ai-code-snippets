cba92472c0779d80aba09575a4e36c9bb67eaa7e
larson-adam <larson.adam12@gmail.com>
2023-09-26 09:19:39 -0700

    added mac setup page sample

---
+++ innovation-week/mac-setup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MacSetup = () => {
+  return (
+    <div>
+      <h2>Mac Setup Instructions</h2>
+      {/* Add your Mac setup instructions here */}
+    </div>
+  );
+};
+
+export default MacSetup;
